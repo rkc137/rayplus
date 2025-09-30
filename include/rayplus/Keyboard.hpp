ea73a2018d81198f9ea51d39239a4e82085d1037
rkc137 <hlopov100@gmail.com>
2025-09-30 23:18:41 +0300

    add all is_* for keyboard

--- include/rayplus/Keyboard.hpp
+++ include/rayplus/Keyboard.hpp
@@ -5,124 +5,129 @@ namespace rayplus::keyboard
 
 enum class Key;
 
+[[nodiscard]] bool is_down(Key key);
+[[nodiscard]] bool is_pressed_repeat(Key key);
+[[nodiscard]] bool is_up(Key key);
+[[nodiscard]] bool is_released(Key key);
 [[nodiscard]] bool is_pressed(Key key);
+// [[nodiscard]] bool is_pressed(Key key);
 
 enum class Key
 {
-    NO_KEY          = 0,        // Key: NULL, used for no key pressed
+    no_key          = 0,        // Key: NULL, used for no key pressed
     // Alphanumeric keys
-    APOSTROPHE      = 39,       // Key: '
-    COMMA           = 44,       // Key: ,
-    MINUS           = 45,       // Key: -
-    PERIOD          = 46,       // Key: .
-    SLASH           = 47,       // Key: /
-    ZERO            = 48,       // Key: 0
-    ONE             = 49,       // Key: 1
-    TWO             = 50,       // Key: 2
-    THREE           = 51,       // Key: 3
-    FOUR            = 52,       // Key: 4
-    FIVE            = 53,       // Key: 5
-    SIX             = 54,       // Key: 6
-    SEVEN           = 55,       // Key: 7
-    EIGHT           = 56,       // Key: 8
-    NINE            = 57,       // Key: 9
-    SEMICOLON       = 59,       // Key: ;
-    EQUAL           = 61,       // Key: =
-    A               = 65,       // Key: A | a
-    B               = 66,       // Key: B | b
-    C               = 67,       // Key: C | c
-    D               = 68,       // Key: D | d
-    E               = 69,       // Key: E | e
-    F               = 70,       // Key: F | f
-    G               = 71,       // Key: G | g
-    H               = 72,       // Key: H | h
-    I               = 73,       // Key: I | i
-    J               = 74,       // Key: J | j
-    K               = 75,       // Key: K | k
-    L               = 76,       // Key: L | l
-    M               = 77,       // Key: M | m
-    N               = 78,       // Key: N | n
-    O               = 79,       // Key: O | o
-    P               = 80,       // Key: P | p
-    Q               = 81,       // Key: Q | q
-    R               = 82,       // Key: R | r
-    S               = 83,       // Key: S | s
-    T               = 84,       // Key: T | t
-    U               = 85,       // Key: U | u
-    V               = 86,       // Key: V | v
-    W               = 87,       // Key: W | w
-    X               = 88,       // Key: X | x
-    Y               = 89,       // Key: Y | y
-    Z               = 90,       // Key: Z | z
-    LEFT_BRACKET    = 91,       // Key: [
-    BACKSLASH       = 92,       // Key: '\'
-    RIGHT_BRACKET   = 93,       // Key: ]
-    GRAVE           = 96,       // Key: `
+    apostrophe      = 39,       // Key: '
+    comma           = 44,       // Key: ,
+    minus           = 45,       // Key: -
+    period          = 46,       // Key: .
+    slash           = 47,       // Key: /
+    zero            = 48,       // Key: 0
+    one             = 49,       // Key: 1
+    two             = 50,       // Key: 2
+    three           = 51,       // Key: 3
+    four            = 52,       // Key: 4
+    five            = 53,       // Key: 5
+    six             = 54,       // Key: 6
+    seven           = 55,       // Key: 7
+    eight           = 56,       // Key: 8
+    nine            = 57,       // Key: 9
+    semicolon       = 59,       // Key: ;
+    equal           = 61,       // Key: =
+    a               = 65,       // Key: A | a
+    b               = 66,       // Key: B | b
+    c               = 67,       // Key: C | c
+    d               = 68,       // Key: D | d
+    e               = 69,       // Key: E | e
+    f               = 70,       // Key: F | f
+    g               = 71,       // Key: G | g
+    h               = 72,       // Key: H | h
+    i               = 73,       // Key: I | i
+    j               = 74,       // Key: J | j
+    k               = 75,       // Key: K | k
+    l               = 76,       // Key: L | l
+    m               = 77,       // Key: M | m
+    n               = 78,       // Key: N | n
+    o               = 79,       // Key: O | o
+    p               = 80,       // Key: P | p
+    q               = 81,       // Key: Q | q
+    r               = 82,       // Key: R | r
+    s               = 83,       // Key: S | s
+    t               = 84,       // Key: T | t
+    u               = 85,       // Key: U | u
+    v               = 86,       // Key: V | v
+    w               = 87,       // Key: W | w
+    x               = 88,       // Key: X | x
+    y               = 89,       // Key: Y | y
+    z               = 90,       // Key: Z | z
+    left_bracket    = 91,       // Key: [
+    backslash       = 92,       // Key: '\'
+    right_bracket   = 93,       // Key: ]
+    grave           = 96,       // Key: `
     // Function keys
-    SPACE           = 32,       // Key: Space
-    ESCAPE          = 256,      // Key: Esc
-    ENTER           = 257,      // Key: Enter
-    TAB             = 258,      // Key: Tab
-    BACKSPACE       = 259,      // Key: Backspace
-    INSERT          = 260,      // Key: Ins
-    DELETE          = 261,      // Key: Del
-    RIGHT           = 262,      // Key: Cursor right
-    LEFT            = 263,      // Key: Cursor left
-    DOWN            = 264,      // Key: Cursor down
-    UP              = 265,      // Key: Cursor up
-    PAGE_UP         = 266,      // Key: Page up
-    PAGE_DOWN       = 267,      // Key: Page down
-    HOME            = 268,      // Key: Home
-    END             = 269,      // Key: End
-    CAPS_LOCK       = 280,      // Key: Caps lock
-    SCROLL_LOCK     = 281,      // Key: Scroll down
-    NUM_LOCK        = 282,      // Key: Num lock
-    PRINT_SCREEN    = 283,      // Key: Print screen
-    PAUSE           = 284,      // Key: Pause
-    F1              = 290,      // Key: F1
-    F2              = 291,      // Key: F2
-    F3              = 292,      // Key: F3
-    F4              = 293,      // Key: F4
-    F5              = 294,      // Key: F5
-    F6              = 295,      // Key: F6
-    F7              = 296,      // Key: F7
-    F8              = 297,      // Key: F8
-    F9              = 298,      // Key: F9
-    F10             = 299,      // Key: F10
-    F11             = 300,      // Key: F11
-    F12             = 301,      // Key: F12
-    LEFT_SHIFT      = 340,      // Key: Shift left
-    LEFT_CONTROL    = 341,      // Key: Control left
-    LEFT_ALT        = 342,      // Key: Alt left
-    LEFT_SUPER      = 343,      // Key: Super left
-    RIGHT_SHIFT     = 344,      // Key: Shift right
-    RIGHT_CONTROL   = 345,      // Key: Control right
-    RIGHT_ALT       = 346,      // Key: Alt right
-    RIGHT_SUPER     = 347,      // Key: Super right
-    KB_MENU         = 348,      // Key: KB menu
+    space           = 32,       // Key: Space
+    escape          = 256,      // Key: Esc
+    enter           = 257,      // Key: Enter
+    tab             = 258,      // Key: Tab
+    backspace       = 259,      // Key: Backspace
+    insert          = 260,      // Key: Ins
+    del             = 261,      // Key: Del
+    right           = 262,      // Key: Cursor right
+    left            = 263,      // Key: Cursor left
+    down            = 264,      // Key: Cursor down
+    up              = 265,      // Key: Cursor up
+    page_up         = 266,      // Key: Page up
+    page_down       = 267,      // Key: Page down
+    home            = 268,      // Key: Home
+    end             = 269,      // Key: End
+    caps_lock       = 280,      // Key: Caps lock
+    scroll_lock     = 281,      // Key: Scroll down
+    num_lock        = 282,      // Key: Num lock
+    print_screen    = 283,      // Key: Print screen
+    pause           = 284,      // Key: Pause
+    f1              = 290,      // Key: F1
+    f2              = 291,      // Key: F2
+    f3              = 292,      // Key: F3
+    f4              = 293,      // Key: F4
+    f5              = 294,      // Key: F5
+    f6              = 295,      // Key: F6
+    f7              = 296,      // Key: F7
+    f8              = 297,      // Key: F8
+    f9              = 298,      // Key: F9
+    f10             = 299,      // Key: F10
+    f11             = 300,      // Key: F11
+    f12             = 301,      // Key: F12
+    left_shift      = 340,      // Key: Shift left
+    left_control    = 341,      // Key: Control left
+    left_alt        = 342,      // Key: Alt left
+    left_super      = 343,      // Key: Super left
+    right_shift     = 344,      // Key: Shift right
+    right_control   = 345,      // Key: Control right
+    right_alt       = 346,      // Key: Alt right
+    right_super     = 347,      // Key: Super right
+    kb_menu         = 348,      // Key: KB menu
     // Keypad keys
-    KP_0            = 320,      // Key: Keypad 0
-    KP_1            = 321,      // Key: Keypad 1
-    KP_2            = 322,      // Key: Keypad 2
-    KP_3            = 323,      // Key: Keypad 3
-    KP_4            = 324,      // Key: Keypad 4
-    KP_5            = 325,      // Key: Keypad 5
-    KP_6            = 326,      // Key: Keypad 6
-    KP_7            = 327,      // Key: Keypad 7
-    KP_8            = 328,      // Key: Keypad 8
-    KP_9            = 329,      // Key: Keypad 9
-    KP_DECIMAL      = 330,      // Key: Keypad .
-    KP_DIVIDE       = 331,      // Key: Keypad /
-    KP_MULTIPLY     = 332,      // Key: Keypad *
-    KP_SUBTRACT     = 333,      // Key: Keypad -
-    KP_ADD          = 334,      // Key: Keypad +
-    KP_ENTER        = 335,      // Key: Keypad Enter
-    KP_EQUAL        = 336,      // Key: Keypad =
+    kp_0            = 320,      // Key: Keypad 0
+    kp_1            = 321,      // Key: Keypad 1
+    kp_2            = 322,      // Key: Keypad 2
+    kp_3            = 323,      // Key: Keypad 3
+    kp_4            = 324,      // Key: Keypad 4
+    kp_5            = 325,      // Key: Keypad 5
+    kp_6            = 326,      // Key: Keypad 6
+    kp_7            = 327,      // Key: Keypad 7
+    kp_8            = 328,      // Key: Keypad 8
+    kp_9            = 329,      // Key: Keypad 9
+    kp_decimal      = 330,      // Key: Keypad .
+    kp_divide       = 331,      // Key: Keypad /
+    kp_multiply     = 332,      // Key: Keypad *
+    kp_subtract     = 333,      // Key: Keypad -
+    kp_add          = 334,      // Key: Keypad +
+    kp_enter        = 335,      // Key: Keypad Enter
+    kp_equal        = 336,      // Key: Keypad =
     // Android key buttons
-    BACK            = 4,        // Key: Android back button
-    MENU            = 5,        // Key: Android menu button
-    VOLUME_UP       = 24,       // Key: Android volume up button
-    VOLUME_DOWN     = 25        // Key: Android volume down button
+    back            = 4,        // Key: Android back button
+    menu            = 5,        // Key: Android menu button
+    volume_up       = 24,       // Key: Android volume up button
+    volume_down     = 25        // Key: Android volume down button
 };
     
 }
--- src/Keyboard.cpp
+++ src/Keyboard.cpp
@@ -5,9 +5,31 @@
 namespace rayplus::keyboard
 {
 
-bool is_pressed(Key key)
+
+bool is_down(Key key)
+{
+    return IsKeyDown(static_cast<int>(key));
+}
+
+bool is_pressed_repeat(Key key)
+{
+    return IsKeyPressedRepeat(static_cast<int>(key));
+}
+
+bool is_up(Key key)
+{
+    return IsKeyUp(static_cast<int>(key));
+}
+
+bool is_released(Key key)
 {
     return IsKeyReleased(static_cast<int>(key));
 }
+
+bool is_pressed(Key key)
+{
+    return IsKeyPressed(static_cast<int>(key));
+}
+
 
 } // namespace rayplus
