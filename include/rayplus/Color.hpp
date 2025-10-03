60ad1ca67558acb2f35ad09aa010c6c5171e25a3
rkc137 <hlopov100@gmail.com>
2025-10-04 00:41:49 +0300

    move const colors in Color namespace

--- include/rayplus/Color.hpp
+++ include/rayplus/Color.hpp
@@ -11,8 +11,7 @@ struct Color
     using u8 = std::uint8_t;
     u8 r{}, g{}, b{}, a{};
 
-    Color() = default;
-    ~Color() = default;
+    constexpr Color() = default;
 
     constexpr Color(u8 r, u8 g, u8 b, u8 a)
     : r(r), g(g), b(b), a(a)
@@ -35,34 +34,62 @@ struct Color
       b(static_cast<u8>(b * 255)),
       a(static_cast<u8>(a * 255))
     {}
+
+    static const Color 
+        lightgray, // Light Gray
+        gray,      // Gray
+        darkgray,  // Dark Gray
+        yellow,    // Yellow
+        gold,      // Gold
+        orange,    // Orange
+        pink,      // Pink
+        red,       // Red
+        maroon,    // Maroon
+        green,     // Green
+        lime,      // Lime
+        darkgreen, // Dark Green
+        skyblue,   // Sky Blue
+        blue,      // Blue
+        darkblue,  // Dark Blue
+        purple,    // Purple
+        violet,    // Violet
+        darkpurple,// Dark Purple
+        beige,     // Beige
+        brown,     // Brown
+        darkbrown, // Dark Brown
+        white,     // White
+        black,     // Black
+        blank,     // Blank (Transparent)
+        magenta,   // Magenta
+        raywhite;  // raylib logo
 };
 
-inline constexpr Color
-lightgray  { 200, 200, 200, 255 },   // Light Gray
-gray       { 130, 130, 130, 255 },   // Gray
-darkgray   { 80, 80, 80, 255 },      // Dark Gray
-yellow     { 253, 249, 0, 255 },     // Yellow
-gold       { 255, 203, 0, 255 },     // Gold
-orange     { 255, 161, 0, 255 },     // Orange
-pink       { 255, 109, 194, 255 },   // Pink
-red        { 230, 41, 55, 255 },     // Red
-maroon     { 190, 33, 55, 255 },     // Maroon
-green      { 0, 228, 48, 255 },      // Green
-lime       { 0, 158, 47, 255 },      // Lime
-darkgreen  { 0, 117, 44, 255 },      // Dark Green
-skyblue    { 102, 191, 255, 255 },   // Sky Blue
-blue       { 0, 121, 241, 255 },     // Blue
-darkblue   { 0, 82, 172, 255 },      // Dark Blue
-purple     { 200, 122, 255, 255 },   // Purple
-violet     { 135, 60, 190, 255 },    // Violet
-darkpurple { 112, 31, 126, 255 },    // Dark Purple
-beige      { 211, 176, 131, 255 },   // Beige
-brown      { 127, 106, 79, 255 },    // Brown
-darkbrown  { 76, 63, 47, 255 },      // Dark Brown
-white      { 255, 255, 255, 255 },   // White
-black      { 0, 0, 0, 255 },         // Black
-blank      { 0, 0, 0, 0 },           // Blank (Transparent)
-magenta    { 255, 0, 255, 255 },     // Magenta
-raywhite   { 245, 245, 245, 255 };   // raylib logo
+inline const Color 
+    Color::lightgray  { 200, 200, 200, 255 },   // Light Gray
+    Color::gray       { 130, 130, 130, 255 },   // Gray
+    Color::darkgray   { 80, 80, 80, 255 },      // Dark Gray
+    Color::yellow     { 253, 249, 0, 255 },     // Yellow
+    Color::gold       { 255, 203, 0, 255 },     // Gold
+    Color::orange     { 255, 161, 0, 255 },     // Orange
+    Color::pink       { 255, 109, 194, 255 },   // Pink
+    Color::red        { 230, 41, 55, 255 },     // Red
+    Color::maroon     { 190, 33, 55, 255 },     // Maroon
+    Color::green      { 0, 228, 48, 255 },      // Green
+    Color::lime       { 0, 158, 47, 255 },      // Lime
+    Color::darkgreen  { 0, 117, 44, 255 },      // Dark Green
+    Color::skyblue    { 102, 191, 255, 255 },   // Sky Blue
+    Color::blue       { 0, 121, 241, 255 },     // Blue
+    Color::darkblue   { 0, 82, 172, 255 },      // Dark Blue
+    Color::purple     { 200, 122, 255, 255 },   // Purple
+    Color::violet     { 135, 60, 190, 255 },    // Violet
+    Color::darkpurple { 112, 31, 126, 255 },    // Dark Purple
+    Color::beige      { 211, 176, 131, 255 },   // Beige
+    Color::brown      { 127, 106, 79, 255 },    // Brown
+    Color::darkbrown  { 76, 63, 47, 255 },      // Dark Brown
+    Color::white      { 255, 255, 255, 255 },   // White
+    Color::black      { 0, 0, 0, 255 },         // Black
+    Color::blank      { 0, 0, 0, 0 },           // Blank (Transparent)
+    Color::magenta    { 255, 0, 255, 255 },     // Magenta
+    Color::raywhite   { 245, 245, 245, 255 };   // raylib logo
 
 } // namespace rayplus
