589c3731d913c9ea72c010d2f63b2bd867c651d6
rkc137 <hlopov100@gmail.com>
2025-11-12 07:18:36 +0300

    vector operators

--- include/rayplus/Vector2.hpp
+++ include/rayplus/Vector2.hpp
@@ -9,7 +9,59 @@ template <typename T>
 requires (std::is_arithmetic_v<T>)
 struct Vector2
 {
-    T x, y;
+    T x{}, y{};
+
+    template <typename TC>
+    requires (std::is_arithmetic_v<TC>)
+    Vector2(TC x, TC y) : x(static_cast<T>(x)), y(static_cast<T>(y)) {}
+
+    Vector2 operator-(const Vector2 &right) { return {
+        x - right.x,
+        y - right.y
+    };}
+
+    Vector2 operator+(const Vector2 &right) { return {
+        x + right.x,
+        y + right.y
+    };}
+
+    Vector2 operator/(const Vector2 &right) { return {
+        x / right.x,
+        y / right.y
+    };}
+
+    Vector2 operator*(const Vector2 &right) { return {
+        x * right.x,
+        y * right.y
+    };}
+
+    template <typename TC>
+    requires (std::is_arithmetic_v<TC>)
+    Vector2 operator-(const TC &right) { return {
+        x - right,
+        y - right
+    };}
+
+    template <typename TC>
+    requires (std::is_arithmetic_v<TC>)
+    Vector2 operator+(const TC &right) { return {
+        x + right,
+        y + right
+    };}
+
+    template <typename TC>
+    requires (std::is_arithmetic_v<TC>)
+    Vector2 operator/(const TC &right) { return {
+        x / right,
+        y / right
+    };}
+
+    template <typename TC>
+    requires (std::is_arithmetic_v<TC>)
+    Vector2 operator*(const TC &right) { return {
+        x * right,
+        y * right
+    };}
 };
 
 } // namespace rayplus
